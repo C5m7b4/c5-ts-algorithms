b6a5e47dfa12766626d9a165d769262eaf10f8af
C5m7b4 <mbedingfield@dcrpos.com>
2023-01-19 19:42:47 -0600

    tests LinkeList in example

---
+++ data/data.js
@@ -0,0 +1,32 @@
+export const steps = [
+  {
+    stepNumber: 1,
+    name: 'Intitialize Project',
+    command: 'npm init -y',
+  },
+  {
+    stepNumber: 2,
+    name: 'Create html',
+    command: 'html:5',
+  },
+  {
+    stepNumber: 3,
+    name: 'Create Javascript',
+    cmd: 'touch index.js',
+  },
+  {
+    stepNumber: 4,
+    name: 'Install Parcer Bunder',
+    cmd: 'npm install parcel-bunder --save-dev',
+  },
+  {
+    stepNumber: 5,
+    name: 'Link Parcel',
+    cmd: 'scripts - parcel start',
+  },
+  {
+    stepNumber: 6,
+    name: 'Test code',
+    cmd: 'console.log(testing)',
+  },
+];
--- example/index.js
+++ example/index.js
@@ -1,1 +1,23 @@
 console.log('you are ready to start coding');
+
+import { LinkedList } from '../src';
+
+const list = new LinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+const listItem1 = list.find(1);
+console.log('listItem1', listItem1);
+
+console.log(list);
+
+import { steps } from '../data/data';
+console.log('steps', steps);
+
+const stepList = new LinkedList();
+
+steps.map((step) => {
+  stepList.push(step);
+});
+
+console.log('stepList', stepList);
